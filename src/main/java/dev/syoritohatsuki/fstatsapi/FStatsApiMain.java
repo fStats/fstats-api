a6cb1a6f8a9857ac304975b3060501adf7cf38b0
syorito-hatsuki <enderdron3@gmail.com>
2022-11-22 22:46:44 +0200

    Added java test file

---
+++ src/main/java/dev/syoritohatsuki/fstatsapi/FStatsApiMain.java
@@ -0,0 +1,18 @@
+package dev.syoritohatsuki.fstatsapi;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+
+public class FStatsApiMain implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        ServerLifecycleEvents.SERVER_STARTED.register(server ->
+                FStatsApi.INSTANCE.sendServerData(server, 1, "fstatsapi")
+        );
+
+        ClientLifecycleEvents.CLIENT_STARTED.register(client ->
+                FStatsApi.INSTANCE.sendClientData(client, 1, "fstatsapi")
+        );
+    }
+}
