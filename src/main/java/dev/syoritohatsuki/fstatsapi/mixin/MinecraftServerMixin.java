3061300edf4a20f2bf92c4e01bc413a2568cb972
syorito-hatsuki <enderdron3@gmail.com>
2023-05-30 01:11:39 +0300

    Added mixin that start data sending

---
+++ src/main/java/dev/syoritohatsuki/fstatsapi/mixin/MinecraftServerMixin.java
@@ -0,0 +1,24 @@
+package dev.syoritohatsuki.fstatsapi.mixin;
+
+import dev.syoritohatsuki.fstatsapi.FStatsApi;
+import net.minecraft.server.MinecraftServer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftServer.class)
+public abstract class MinecraftServerMixin {
+
+    @Shadow
+    public abstract String getVersion();
+
+    @Shadow
+    public abstract boolean isOnlineMode();
+
+    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;createMetadata()Lnet/minecraft/server/ServerMetadata;", ordinal = 0), method = "runServer")
+    private void afterSetupServer(CallbackInfo info) {
+        FStatsApi.INSTANCE.sendMetricRequest(getVersion(), isOnlineMode());
+    }
+}
