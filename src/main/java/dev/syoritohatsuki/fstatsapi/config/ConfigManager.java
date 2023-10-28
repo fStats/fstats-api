45ab0c34754264c455029ca506ba04766f4e2963
syorito-hatsuki <enderdron3@gmail.com>
2023-10-28 17:56:28 +0300

    Added fallback realization for config reading

--- src/main/java/dev/syoritohatsuki/fstatsapi/config/ConfigManager.java
+++ src/main/java/dev/syoritohatsuki/fstatsapi/config/ConfigManager.java
@@ -9,14 +9,15 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import static dev.syoritohatsuki.fstatsapi.FStatsApi.MOD_ID;
+import static dev.syoritohatsuki.fstatsapi.FStatsApi.logger;
 
 public final class ConfigManager {
 
-    private final File configDir = Paths.get("", "config", MOD_ID).toFile();
+    private static final File configDir = Paths.get("", "config", MOD_ID).toFile();
 
-    private final File configFile = new File(configDir, "upgrades.json");
+    private static final File configFile = new File(configDir, "config.json");
 
-    private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
     public ConfigManager() {
         if (!configDir.exists()) configDir.mkdirs();
@@ -30,11 +31,19 @@ public final class ConfigManager {
         }
     }
 
-    public Config read() {
+    public static Config read() {
         try {
             return gson.fromJson(Files.readString(configFile.toPath()), Config.class);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            logger.error("Can't read config");
+            try {
+                logger.info("Backup config...");
+                Files.copy(configFile.toPath(), new File(configDir, "backup_config.json").toPath());
+                Files.writeString(configFile.toPath(), gson.toJson(new Config(true, false)));
+                return gson.fromJson(Files.readString(configFile.toPath()), Config.class);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
         }
     }
 
