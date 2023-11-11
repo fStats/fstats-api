f08d67f2e8fec8ab5d1a64bd30ea70abc0f7efaa
syorito-hatsuki <enderdron3@gmail.com>
2023-11-12 01:34:56 +0200

    Added a console message for an inform server owner about fStats existing

--- src/main/java/dev/syoritohatsuki/fstatsapi/mixin/MinecraftServerMixin.java
+++ src/main/java/dev/syoritohatsuki/fstatsapi/mixin/MinecraftServerMixin.java
@@ -20,6 +20,12 @@ public abstract class MinecraftServerMixin {
 
     @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;createMetadata()Lnet/minecraft/server/ServerMetadata;", ordinal = 0), method = "runServer")
     private void afterSetupServer(CallbackInfo info) {
+
+        FStatsApi.logger.info("-----[ \u001B[36m\u001B[1mfStats\u001B[0m ]-----");
+        FStatsApi.logger.info("\u001B[33mOn server exist mod that collect metric data about mods usage for few author\u001B[0m");
+        FStatsApi.logger.info("\u001B[33mThat mod work like bStats, no need to panic. U can disable it config if you want\u001B[0m");
+        FStatsApi.logger.info("--------------------");
+
         ConfigManager.init();
 
         if (ConfigManager.read().enabled()) {
